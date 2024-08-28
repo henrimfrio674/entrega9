23aab9cffb7d250c237713a6cbf060b741de1af0
henrimfrio674 <47890265+henrimfrio674@users.noreply.github.com>
2024-08-28 10:23:16 -0300

    Create ex1.java

---
+++ ex1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SomaComparacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o número A: ");
+        int A = scanner.nextInt();
+        
+        System.out.print("Digite o número B: ");
+        int B = scanner.nextInt();
+        
+        System.out.print("Digite o número C: ");
+        int C = scanner.nextInt();
+        
+        int soma = A + B;
+        
+        if (soma > C) {
+            System.out.println(A + " + " + B + " = " + soma + " > " + C);
+            System.out.println("A Soma de A + B é Maior do que C");
+        } else if (soma < C) {
+            System.out.println(A + " + " + B + " = " + soma + " < " + C);
+            System.out.println("A Soma de A + B é Menor do que C");
+        } else {
+            System.out.println(A + " + " + B + " = " + soma + " = " + C);
+            System.out.println("A Soma de A + B é Igual a C");
+        }
+        
+        scanner.close();
+    }
+}
